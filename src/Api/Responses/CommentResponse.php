81c8d9fdc0216dcd88223f48e49748a0a43695aa
Гранд Юрий <grandya@gpbl.ru>
2024-02-05 12:04:32 +0300

    Добавлен CommentResponse

---
+++ src/Api/Responses/CommentResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BugrovWeb\YandexTracker\Api\Responses;
+
+use BugrovWeb\YandexTracker\Api\ClientResponse;
+
+/**
+ * Комментарий
+ * 
+ * @method \BugrovWeb\YandexTracker\Api\Entities\CommentEntity getResponseEntity()
+ */
+class CommentResponse extends ClientResponse
+{
+}
